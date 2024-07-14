ec67f2f9127c217c62d006d58aba2b360dd3054d
vernonlim <vernonlim2004@gmail.com>
2024-07-14 21:39:05 +0800

    add graphing toggle:

--- osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyCalculator.cs
+++ osu.Game.Rulesets.Mania/Difficulty/ManiaDifficultyCalculator.cs
@@ -99,7 +99,7 @@ namespace osu.Game.Rulesets.Mania.Difficulty
 
         protected override Skill[] CreateSkills(IBeatmap beatmap, Mod[] mods, double clockRate)
         {
-            return new Skill[] { new SunnySkill(mods, ((ManiaBeatmap)beatmap).TotalColumns, beatmap.Difficulty.OverallDifficulty, granularity, ((ManiaBeatmap)beatmap).HitObjects.Count)  };
+            return new Skill[] { new SunnySkill(mods, ((ManiaBeatmap)beatmap).TotalColumns, beatmap.Difficulty.OverallDifficulty, granularity, ((ManiaBeatmap)beatmap).HitObjects.Count, beatmap.BeatmapInfo)  };
         }
 
         protected override Mod[] DifficultyAdjustmentMods
