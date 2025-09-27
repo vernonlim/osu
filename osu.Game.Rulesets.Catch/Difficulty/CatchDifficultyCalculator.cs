3e3f2a3b14ed379a6ff0728cbdb609f6db8eaac9
vernonlim <vernonlim2004@gmail.com>
2025-09-27 06:09:04 +0000

    Toggle debug flag

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -68,7 +68,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty
             }
 
             // Debug code
-            bool debug = true;
+            bool debug = false;
 
             if (debug)
             {
