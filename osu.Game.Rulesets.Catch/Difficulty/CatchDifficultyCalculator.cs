0ebfcf4e092e50a24c3d3293144d90a4058a80bd
vernonlim <vernonlim2004@gmail.com>
2025-10-01 18:31:32 +0000

    Change difficulty multiplier

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -20,7 +20,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty
 {
     public class CatchDifficultyCalculator : DifficultyCalculator
     {
-        private const double difficulty_multiplier = 2;
+        private const double difficulty_multiplier = 1;
 
         private float catcherWidth;
 
