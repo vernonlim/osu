0231f82be06810bc979246d3c73c6c7ee1726529
vernonlim <vernonlim2004@gmail.com>
2025-10-18 15:06:37 +0000

    Adjust SR multiplier again

--- osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchDifficultyCalculator.cs
@@ -65,7 +65,8 @@ namespace osu.Game.Rulesets.Catch.Difficulty
 
             // temporary rescaling to help with testing
             const double scaling_point = 5.8;
-            sr = sr < scaling_point ? sr : sr * Math.Min(1 + Math.Max((sr - scaling_point) / 2.5, 0) * 0.4, 1.4);
+            double multiplier = Math.Max(Math.Min(1 + Math.Max((sr - scaling_point) / 2.5, 0) * 0.4, 1.35), Math.Min(1 + Math.Max((sr - 3.0) / 3.0, 0) * 0.15, 1.05));
+            sr = sr * multiplier;
 
             CatchDifficultyAttributes attributes = new CatchDifficultyAttributes
             {
