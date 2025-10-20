a33f161402b6e5d397798ab42a1a6753fcca086c
trig0n03 <104526558+trig0n03@users.noreply.github.com>
2025-10-20 23:51:26 +0200

    Slightly changed formula for map's length for testing purposes.

--- osu.Game.Rulesets.Catch/Difficulty/CatchPerformanceCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchPerformanceCalculator.cs
@@ -43,8 +43,8 @@ namespace osu.Game.Rulesets.Catch.Difficulty
             double totalActions = ((CatchDifficultyAttributes)attributes).TotalActions;
 
             double lengthBonus =
-                0.95 + 0.3 * Math.Min(1.0, totalActions / 1250.0) +
-                (totalActions > 1250 ? Math.Log10(totalActions / 1250.0) * 0.475 : 0.0);
+                0.95 + 0.3 * Math.Min(1.0, totalActions / 1200.0) +
+                (totalActions > 1200 ? Math.Log10(totalActions / 1200.0) * 0.45 : 0.0);
             value *= lengthBonus;
 
             value *= Math.Pow(0.97, numMiss);
