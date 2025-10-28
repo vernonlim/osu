884a63bd8b4bed0f16520842e8946ad8dbefa202
vernonlim <vernonlim2004@gmail.com>
2025-10-29 02:28:00 +0800

    Add basic values for StrainDecayBase and SkillMultiplier

--- osu.Game.Rulesets.Catch/Difficulty/Skills/Precision.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Skills/Precision.cs
@@ -12,9 +12,9 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Skills
 {
     public class Precision : StrainDecaySkill
     {
-        protected override double SkillMultiplier => 1;
+        protected override double SkillMultiplier => 0.3;
 
-        protected override double StrainDecayBase => 0.00;
+        protected override double StrainDecayBase => 0.10;
 
         public Precision(Mod[] mods)
             : base(mods)
