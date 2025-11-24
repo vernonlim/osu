b09107b5f2a8cabb7af7f8b23e7afe556f032f76
trig0n03 <104526558+trig0n03@users.noreply.github.com>
2025-11-24 12:28:52 +0100

    Precision correction improvement and nerf

--- osu.Game.Rulesets.Catch/Difficulty/Preprocessing/Preprocessors/CatchDifficultyPreprocessor.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Preprocessing/Preprocessors/CatchDifficultyPreprocessor.cs
@@ -248,7 +248,7 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Preprocessing.Preprocessors
 
                     double standstillTime = CatchPreprocessingUtils.CalculatePotentialStandstillEffectiveTime(note, next);
 
-                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, note.DeltaTime, note.CatcherWidth, prevForwardCatcherPosition);
+                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, prevForwardCatcherPosition, note.DeltaTime, note.CatcherWidth, PatternType.HyperjumpAfterJump, note.IsMovingRight);
                     data.PrecisionCorrection = precisionCorrection;
 
                     data.EffectiveTime = standstillTime * (precisionCorrection - 1) + data.EffectiveTime * (2 - precisionCorrection);
@@ -261,12 +261,28 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Preprocessing.Preprocessors
                     double acceleratingTime = (data.Directionize(prev.Position - next.Position) - note.HalfCatcherWidth + 2 * note.StartTime) / 2.0;
                     double? rawPrecision = calculateRawPrecision(note, prev, next, PatternType.Jumps);
 
-                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, note.DeltaTime, note.CatcherWidth, prevForwardCatcherPosition);
+                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, prevForwardCatcherPosition, note.DeltaTime, note.CatcherWidth, PatternType.Jumps, note.IsMovingRight);
                     data.PrecisionCorrection = precisionCorrection;
 
                     data.EffectiveTime = acceleratingTime * (precisionCorrection - 1) + data.EffectiveTime * (2 - precisionCorrection);
 
                     return precisionCorrection * rawPrecision;
+                }
+
+                case PatternType.PotentialStandstill:
+                {
+                    double? rawPrecision = calculateRawPrecision(note, prev, next, PatternType.PotentialStandstill);
+                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, prevForwardCatcherPosition, note.DeltaTime, note.CatcherWidth, PatternType.PotentialStandstill, note.IsMovingRight);
+                    
+                    return precisionCorrection / 2.0 * rawPrecision;
+                }
+
+                case PatternType.AcceleratingStream:
+                {
+                    double? rawPrecision = calculateRawPrecision(note, prev, next, PatternType.AcceleratingStream);
+                    double precisionCorrection = CatchPreprocessingUtils.CalculatePrecisionCorrection(note.Position, prevForwardCatcherPosition, note.DeltaTime, note.CatcherWidth, PatternType.AcceleratingStream, note.IsMovingRight);
+                    
+                    return precisionCorrection / 2.0 * rawPrecision;
                 }
 
                 default:
--- osu.Game.Rulesets.Catch/Difficulty/Preprocessing/Utils/CatchPreprocessingUtils.cs
+++ osu.Game.Rulesets.Catch/Difficulty/Preprocessing/Utils/CatchPreprocessingUtils.cs
@@ -208,20 +208,26 @@ namespace osu.Game.Rulesets.Catch.Difficulty.Preprocessing.Utils
             }
         }
 
-        public static double CalculatePrecisionCorrection(double position, double deltaTime, double catcherWidth, double furthestForward)
+        public static double CalculatePrecisionCorrection(double position, double prevForwardPosition, double deltaTime, double catcherWidth, PatternType pattern, bool isMovingRight)
         {
             const double catcherExponent = 1.0;
             const double timeExponent = 1.0;
+            const double distanceExponent = 0.7;
+            const double maxCorrection = 0.5;
 
-            double distance = Math.Max(0.0, position - furthestForward);
+            if (deltaTime == 0)
+                return 1.0 + maxCorrection;
 
-            double standingTime = Math.Max(0.0, deltaTime - distance);
+            double minDistance = isMovingRight ? Math.Max(0.0, position - prevForwardPosition) : Math.Max(0.0, prevForwardPosition - position);
+
+            double standingTime = Math.Max(0.0, deltaTime - minDistance);
 
             double catcherRatio = Math.Min(1.0, 2.0 * standingTime / catcherWidth); // If player can stand for more than c/2, they has to wait until being able to catch the note - precision correction doesn't grow
-            double timeRatio = Math.Min(1.0, 2.0 * standingTime / deltaTime);
+            // double timeRatio = Math.Min(1.0, 2.0 * standingTime / deltaTime);
+            double distanceRatio = Math.Min(1.0, minDistance / catcherWidth);
 
-            double value = 1.0 + 0.5 * Math.Pow(catcherRatio, catcherExponent) + 0.5 * Math.Pow(timeRatio, timeExponent) ;
-            return Math.Clamp(value, 1.0, 2.0);
+            double value = 1.0 + maxCorrection * (0.5 * Math.Pow(catcherRatio, catcherExponent) + 0.5 * Math.Pow(1.0 - distanceRatio, distanceExponent));
+            return Math.Clamp(value, 1.0, 1.0 + maxCorrection);
         }
 
         public static double? CalculateCurvedStackProbability(CatchDifficultyHitObject note, CatchDifficultyHitObject prev, CatchDifficultyHitObject next, PatternType type)
